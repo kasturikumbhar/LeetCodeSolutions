4acc3eaf27729c6cb9ecc170a4f4acb08da86a48
kasturikumbhar <43609184+kasturikumbhar@users.noreply.github.com>
2026-05-24 17:25:29 +0530

    ue monotonic stack
    observe when next block is smaller than current block, the area maximization fails, so that breaking point, 
    stack holds all increasing value, as soon as u encounter smaller value pop from stack cal area 
    keep track of max area so far , block is of width1 , so its height of block* (distance from smaller block to curb block >> for width)

---
+++ monotonic-stack/largest_rectangle_area_in_histogram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0)
+        stack=[]
+        maxarea=0
+
+        for i,height in enumerate(heights):
+            while stack and height <heights[stack[-1]]:
+                stacktop=stack.pop()
+                leftboundary= stack[-1] if stack else -1 ## leftboundary is prev block
+                area=heights[stacktop]* (i-stacktop-1) 
+                maxarea=max(maxarea,area)
+            stack.append(i)
+        return maxarea
+        
