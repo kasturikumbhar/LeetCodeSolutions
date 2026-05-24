40d1d0e0537174064feea12570af8a0ee79dd457
kasturikumbhar <43609184+kasturikumbhar@users.noreply.github.com>
2026-05-24 19:58:12 +0530

    Update climbing_stairs.py

--- DP/climbing_stairs.py
+++ DP/climbing_stairs.py
@@ -5,9 +5,7 @@ class Solution:
         curr=0
         if n==0 or n==1:
             return 1
-        for i in range(1,n+1):
-            if i <2:
-                continue
+        for i in range(2,n+1):
             curr=prev1+prev2
             prev2=prev1
             prev1=curr
