e0920b316aa6894aa3aeaf7a145076e98328b3d3
kasturikumbhar <43609184+kasturikumbhar@users.noreply.github.com>
2026-05-25 19:32:34 +0530

    for amount what coin can be used and the diff amt
    dp[amt]=min(dp[amt],1+dp[amt-coin])

---
+++ DP/coin_change.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount==0:
+            return 0
+        
+        dp=[ float('inf')] * (amount+1)
+        dp[0]=0
+        for i in range(1,amount+1):
+            for c in coins:
+                if i >= c:
+                    dp[i]=min(dp[i],1+dp[i-c])
+        return dp[-1] if dp[-1]!=float('inf') else -1
+        
